88f6bb4a8be9af56037863f686bfd3d4fa9471f2
kim-taewoo <acoustic0419@gmail.com>
2020-01-20 18:07:46 +0900

    doing 37

--- Algorithm/inflearn_cpp/myCode/37.cpp
+++ Algorithm/inflearn_cpp/myCode/37.cpp
@@ -1,6 +1,55 @@
-#include <stdio.h?
+#include <stdio.h>
+
+void miss(int a[], int s, int tmp)
+{
+    for (size_t i = s-1; i >= 1; i--)
+    {
+        a[i] = a[i-1];
+    }
+    a[0] = tmp;
+}
+
+void hit(int a[], int tmp, int pos)
+{
+    for (size_t i = pos; i >=1; i--)
+    {
+        a[i] = a[i-1];
+    }
+    a[0] = tmp;    
+}
+
 int main()
 {
-    
+    int a[11] = {0,};
+    int s, n , tmp, pos;
+    scanf("%d %d", &s, &n);
+
+    for (size_t i = 0; i < n; i++)
+    {
+        scanf("%d", &tmp);
+        pos = -1;
+        for (size_t j = 0; j < s; j++)
+        {
+            if (a[j] == tmp)
+            {
+                pos = j;
+                break;
+            }
+        }
+        if (pos == -1) miss(a, s, tmp);
+        else hit(a, tmp, pos);
+
+        // for (size_t k = 0; k < s; k++)
+        // {
+        //     printf("%d ", a[k]);
+        // }
+        // printf("\n");
+    }
+
+    for (size_t k = 0; k < s; k++)
+    {
+        printf("%d ", a[k]);
+    }
+
     return 0;
 }
