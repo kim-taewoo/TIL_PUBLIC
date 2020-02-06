b0fd62e84aa32837c778f65e9c12a261974e5e1f
kim-taewoo <acoustic0419@gmail.com>
2020-02-06 23:51:35 +0900

    an inflearn 64

--- Algorithm/inflearn_cpp/myCode/64_경로탐색(dfs).cpp
+++ Algorithm/inflearn_cpp/myCode/64_경로탐색(dfs).cpp
@@ -1,36 +1,38 @@
 #include <stdio.h>
 int n, cnt = 0;
-int a[30][30];
-int chk[30];
+int map[21][21];
+int chk[21];
 
-void dfs(int level)
+void dfs(int v)
 {
-    if (level == n)
+    if (v==n)
     {
         cnt++;
-        return;
     }
-    for (size_t i = 1; i <= n; i++)
+    else
     {
-        if (a[level][i] && chk[i] == 0)
+        for (size_t i = 1; i <= n; i++)
         {
-            chk[i] = 1;
-            dfs(i);
-            chk[i] = 0;
+            if (chk[i] == 0 && map[v][i])
+            {
+                chk[i] = 1;
+                dfs(i);
+                chk[i] = 0;
+            }
         }
     }
-    
 }
 
 int main()
 {
-    int m, c, d;
+    int m, a, b;
     scanf("%d %d", &n, &m);
     for (size_t i = 1; i <= m; i++)
     {
-        scanf("%d %d", &c, &d);
-        a[c][d] = 1;
+        scanf("%d %d", &a, &b);
+        map[a][b] = 1;
     }
+    // 1번 정점에서 시작한다는 말이 문제에 있었음!
     chk[1] = 1;
     dfs(1);
     printf("%d\n", cnt);
