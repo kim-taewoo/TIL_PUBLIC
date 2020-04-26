df50be7b64b57b67a6eafb82188c9f4688c4a8e7
kim-taewoo <acoustic0419@gmail.com>
2020-04-27 01:44:58 +0900

    made client side of it logger pjt

--- Javascript/React/udemy-brad/it-logger/src/App.js
+++ Javascript/React/udemy-brad/it-logger/src/App.js
@@ -2,6 +2,10 @@ import React, { useEffect, Fragment } from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
 import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -17,6 +21,10 @@ const App = () => {
       <SearchBar />
       <div className='container'>
         <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </Fragment>
