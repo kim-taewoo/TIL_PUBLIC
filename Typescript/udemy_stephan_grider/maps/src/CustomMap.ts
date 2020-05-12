fc7756abad5ddb7f5bf8598bf3e36a3beb73119e
kim-taewoo <acoustic0419@gmail.com>
2020-05-13 01:44:18 +0900

    removed .cache and dist folder from git by using command git rm -r --cached foldername. And also finished a typescript pjt

--- Typescript/udemy_stephan_grider/maps/src/CustomMap.ts
+++ Typescript/udemy_stephan_grider/maps/src/CustomMap.ts
@@ -1,5 +1,16 @@
-import { User } from './User';
-import { Company } from './Company';
+// interface 를 사용함으로써 필요 없어진 import
+// import { User } from './User';
+// import { Company } from './Company';
+
+// Instructions to every other class
+// on how they can be an argument to 'addMarker'
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+}
 
 export class CustomMap {
   private googleMap: google.maps.Map;
@@ -14,23 +25,30 @@ export class CustomMap {
     });
   }
 
-  addUserMarker(user: User): void {
-    new google.maps.Marker({
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: user.location.lat,
-        lng: user.location.lng,
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
       },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+      infoWindow.open(this.googleMap, marker);
     });
   }
 
-  addCompanyMarker(company: Company): void {
-    new google.maps.Marker({
-      map: this.googleMap,
-      position: {
-        lat: company.location.lat,
-        lng: company.location.lng,
-      },
-    });
-  }
+  // addCompanyMarker(company: Company): void {
+  //   new google.maps.Marker({
+  //     map: this.googleMap,
+  //     position: {
+  //       lat: company.location.lat,
+  //       lng: company.location.lng,
+  //     },
+  //   });
+  // }
 }
