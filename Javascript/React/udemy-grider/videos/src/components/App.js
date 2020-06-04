9c24a195beab036a192dd0f72c92a931597eedbd
kim-taewoo <acoustic0419@gmail.com>
2020-06-04 23:32:27 +0900

    done youtube clone with youtube data api with react

--- Javascript/React/udemy-grider/videos/src/components/App.js
+++ Javascript/React/udemy-grider/videos/src/components/App.js
@@ -1,26 +1,59 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 const KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export class App extends Component {
-  onTermSubmit = (term) => {
-    youtube.get('/search', {
+  state = {
+    videos: [],
+    selectedVideo: null,
+  };
+
+  componentDidMount() {
+    this.onTermSubmit('buildings')
+  }
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
       params: {
         part: 'snippet',
         type: 'video',
         maxResult: 5,
         key: `${KEY}`,
         q: term,
-      }
+      },
     });
+
+    this.setState({ 
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className='ui container'>
         <SearchBar onTermSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
