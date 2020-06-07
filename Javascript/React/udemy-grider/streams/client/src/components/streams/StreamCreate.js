02b961af965a2cf0065ff37b6fb74be4a5a5dd86
kim-taewoo <acoustic0419@gmail.com>
2020-06-08 03:00:32 +0900

    studied redux-form

--- Javascript/React/udemy-grider/streams/client/src/components/streams/StreamCreate.js
+++ Javascript/React/udemy-grider/streams/client/src/components/streams/StreamCreate.js
@@ -1,11 +1,70 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return (
-    <div>
-      StreamCreate
-    </div>
-  )
+export class StreamCreate extends Component {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      console.log(touched, error);
+      return (
+        <div className='ui error message'>
+          <div className='header'>{error}</div>
+        </div>
+      );
+    }
+  }
+
+  // formProps 를 destructuring 해서 많이 쓴다.
+  renderInput = (formProps) => {
+    // console.log(formProps);
+    const className = `field ${formProps.meta.error && formProps.meta.touched ? 'error' : ''}`
+    return (
+      <div className={className}>
+        <label htmlFor=''>{formProps.label}</label>
+        <input {...formProps.input} autoComplete='off' />
+        {this.renderError(formProps.meta)}
+      </div>
+    );
+  };
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    // console.log(this.props);
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className='ui form error'
+      >
+        <Field name='title' component={this.renderInput} label='Enter Title' />
+        <Field
+          name='description'
+          component={this.renderInput}
+          label='Enter Description'
+        />
+        <button className='ui button primary'>Submit</button>
+      </form>
+    );
+  }
 }
 
-export default StreamCreate
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = '제목은 반드시 입력해야 해요';
+  }
+
+  if (!formValues.description) {
+    errors.description = '내용은 반드시 입력해야 해요!';
+  }
+
+  return errors;
+};
+
+export default reduxForm({
+  // 이 form 이름 붙이기
+  form: 'streamCreate',
+  validate,
+})(StreamCreate);
--- Javascript/React/udemy-grider/streams/client/src/reducers/index.js
+++ Javascript/React/udemy-grider/streams/client/src/reducers/index.js
@@ -1,6 +1,8 @@
-import {combineReducers} from 'redux';
-import authReducer from './authReducer'
+import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import authReducer from './authReducer';
 
-export default combineReducers( {
-  auth: authReducer
-})
+export default combineReducers({
+  auth: authReducer,
+  form: formReducer,
+});
