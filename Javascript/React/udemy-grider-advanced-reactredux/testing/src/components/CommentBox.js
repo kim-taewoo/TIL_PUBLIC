91335efb0aedf0362031b988670e30bca2a8d2b1
kim-taewoo <acoustic0419@gmail.com>
2020-06-20 18:05:41 +0900

    Studying testing in React with Stephen grider's udemy course

---
+++ Javascript/React/udemy-grider-advanced-reactredux/testing/src/components/CommentBox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CommentBox = () => {
+  return (
+    <div>
+      Comment Box Comp
+    </div>
+  )
+}
+
+export default CommentBox
---
+++ Javascript/React/udemy-grider-advanced-reactredux/testing/src/components/CommentList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CommentList = () => {
+  return (
+    <div>
+      Comment List Comp
+    </div>
+  )
+}
+
+export default CommentList
