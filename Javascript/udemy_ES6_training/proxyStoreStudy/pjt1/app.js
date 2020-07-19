41cb4a99605240114903ec9e5cd1160ac8f9ce07
kim-taewoo <acoustic0419@gmail.com>
2020-07-20 00:21:14 +0900

    DOM diff

--- Javascript/udemy_ES6_training/proxyStoreStudy/pjt1/app.js
+++ Javascript/udemy_ES6_training/proxyStoreStudy/pjt1/app.js
@@ -1,3 +1,63 @@
+const getNodeType = function(node) {
+  if (node.nodeType === 3) return 'text';
+  if (node.nodeType === 8) return 'comment';
+  return node.tagName.toLowerCase();
+}
+
+const getNodeContent = function (node) {
+  if (node.childNodes && node.childNodes.length > 0) return null;
+  return node.textContent;
+}
+
+const stringToHTML = function (str) {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(str, 'text/html');
+  return doc.body;
+}
+
+const diff = function(template, elem) {
+  const domNodes = [...elem.childNodes];
+  const templateNodes = [...template.childNodes]
+
+  let count = domNodes.length - templateNodes.length;
+  while (count > 0) {
+    domNodes[domNodes.length - count].remove();
+  }
+
+  templateNodes.forEach((node, index) => {
+    if (!domNodes[index]) {
+      elem.appendChild(node.cloneNode(true))
+      return;
+    }
+
+    if (getNodeType(node) !== getNodeType(domNodes[index])) {
+      domNodes[index].parentNode.replaceChild(node.cloneNode(true), domNodes[index])
+      return;
+    }
+
+    const templateContent = getNodeContent(node);
+    if (templateContent && templateContent !== getNodeContent(domNodes[index])) {
+      domNodes[index].textContent = tempalteContent;
+    }
+
+    if (domNodes[index].childNodes.length > 0 && node.childNodes.length < 1) {
+      domNodes[index].innerHTML = '';
+      return;
+    }
+
+    if (domNodes[index].childNodes.length < 1 && node.childNodes.length > 0) {
+      const fragment = document.createDocumentFragment();
+      diff(node, fragment);
+      domNodes[index].appendChild(fragment);
+      return;
+    }
+
+    if (node.childNodes.length > 0) {
+      diff(node, domNodes[index]);
+    }
+  })
+}
+
 const debounceRender = function (instance) {
   // if there's a pending render, cancel it
   if (instance.debounce) {
@@ -60,7 +120,10 @@ class Component {
   }
 
   render() {
-    this.elem.innerHTML = this.template(this.data);
+    // this.elem.innerHTML = this.template(this.data);
+    const templateHTML = stringToHTML(this.template(this.data))
+
+    diff(templateHTML, this.elem)
   }
 }
 
