d800acdadd436c0257af8b51d9137147d032af5f
kim-taewoo <acoustic0419@gmail.com>
2020-08-26 00:02:25 +0900

    before learning references

--- firebase/firebase-udemy-revolution/src/components/cars/index.js
+++ firebase/firebase-udemy-revolution/src/components/cars/index.js
@@ -1,22 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { db } from '../../utils/firebase';
 
-import { firebaseLooper } from '../../utils/tools'
+import { firebaseLooper } from '../../utils/tools';
 
 export class Cars extends Component {
+  state = {
+    cars: null,
+  };
+
   componentDidMount() {
-    db.collection('cars').get().then(snapshot => {
-      const cars = firebaseLooper(snapshot);
-      console.log(cars);
-    }).catch(e => console.error(e))
+    db.collection('cars')
+      .get()
+      .then((snapshot) => {
+        const cars = firebaseLooper(snapshot);
+        this.setState({ cars });
+      })
+      .catch((e) => console.error(e));
   }
+
+  handleCarData = (cars) =>
+    cars
+      ? cars.map((data, i) => (
+          <tr key={i}>
+            <td>{data.id}</td>
+            <td>{data.brand}</td>
+            <td>{data.color}</td>
+          </tr>
+        ))
+      : null;
+
   render() {
+    const cars = this.state.cars;
     return (
-      <div>
-        
-      </div>
-    )
+      <>
+        <table className='table table-dark'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Brand</th>
+              <th>Color</th>
+            </tr>
+          </thead>
+          <tbody>{this.handleCarData(cars)}</tbody>
+        </table>
+      </>
+    );
   }
 }
 
-export default Cars
+export default Cars;
--- firebase/firebase-udemy-revolution/src/utils/tools.js
+++ firebase/firebase-udemy-revolution/src/utils/tools.js
@@ -1,5 +1,5 @@
 export const firebaseLooper = (snapshot) => {
-  // 아래 코드는 파이어베이스가 자체 제공하는 forEach 를 사용해서 할 수도 있다.
+  // 아래 코드는 파이어베이스가 자체 제공하는 forEach 를 사용해서 `docs` 부분을 생략할 수도 있다.
   // 즉, snapshot.forEach((doc) => ({id: doc.id, ...doc.data()}))
   // 여기서 forEach 는 자바스크립트에서 제공하는 기본 forEach 가 아님을 알아야 한다!
   const data = snapshot.docs.map((doc)=> ({id: doc.id, ...doc.data()}))
