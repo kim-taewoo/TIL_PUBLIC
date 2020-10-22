5b6ea9285b3a64e97a316c1e056c0a7f412cdbb6
kim-taewoo <acoustic0419@gmail.com>
2020-10-23 01:03:24 +0900

    cpp 더 맵게 주석 추가

--- Algorithm/programmers/코딩테스트대비C++/더맵게_힙.cpp
+++ Algorithm/programmers/코딩테스트대비C++/더맵게_힙.cpp
@@ -6,7 +6,10 @@ using namespace std;
 
 int solution(vector<int> scoville, int K) {
   int answer = 0;
-  priority_queue<int, vector<int>, greater<int>> pq;
+  // 최소힙. 3번째 인자가 greater 인 이유는, greater, 즉 클수록 true 를 반환해서 왼쪽으로 정렬되는데,
+  // pop 했을 때 먼저 빠져야 하는 게 제일 작은 것이어야 하기 때문에, 작은 게 오른쪽으로 가는
+  // greater 로 정렬해야 한다.
+  priority_queue<int, vector<int>, greater<int>> pq; 
   for (auto& i: scoville) pq.push(i);
   while (1) {
     int min1 = pq.top(); pq.pop();
