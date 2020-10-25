ed835eca9a044b06e51aa1315c7bdbbd426140be
kim-taewoo <acoustic0419@gmail.com>
2020-10-25 21:56:07 +0900

    간단한 spa 라우팅 구현해보기(without react-router-dom)

--- Javascript/React/실전리액트프로그래밍책/cra-test/src/App.js
+++ Javascript/React/실전리액트프로그래밍책/cra-test/src/App.js
@@ -1,21 +1,55 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
 
 export default function App() {
+  const [pageName, setPageName] = useState('');
   useEffect(() => {
-    window.onpopstate = function(e) {
-      console.log(`location: ${document.location}, state: ${e.state.name}`);
-    }
+    window.onpopstate = (e) => {
+      setPageName(e.state);
+      console.log(`location: ${document.location}, state: ${e.state}`); // 객체도 가능.
+    };
   }, []);
+
+  function onClick1() {
+    const pageName = 'page1';
+    window.history.pushState(pageName, '', '/page1');
+    setPageName(pageName);
+  }
+
+  function onClick2() {
+    const pageName = 'page2';
+    window.history.pushState(pageName, '', '/page2');
+    setPageName(pageName);
+  }
 
   return (
     <>
-      <button onClick={() => window.history.pushState({name:'v1'}, '', '/page1')}>
-        page1
-      </button>
-      <button onClick={() => window.history.pushState('v2', '', '/page2')}>
-        page2
-      </button>
+      <button onClick={onClick1}>page1</button>
+      <button onClick={onClick2}>page2</button>
+      {!pageName && <Home />}
+      {pageName === 'page1' && <Page1 />}
+      {pageName === 'page2' && <Page2 />}
     </>
   );
 }
 
+function Home() {
+  return <h2>홈페이지</h2>;
+}
+
+function Page1() {
+  return (
+    <div>
+      <h2>page1</h2>
+      <p>{document.location.href}</p>
+    </div>
+  );
+}
+
+function Page2() {
+  return (
+    <div>
+      <h2>page2</h2>
+      <p>{document.location.href}</p>
+    </div>
+  );
+}
